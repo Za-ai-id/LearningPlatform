2d12e76fe0f1e39e4ae3effd0549b550e5f60741
ZaID <MuhammedZ@hexaware.com>
2025-07-03 11:42:08 +0530

    postgres db &removed sqlite db

--- grapeexpections-backend/learningplatform/myproj/settings.py
+++ grapeexpections-backend/learningplatform/myproj/settings.py
@@ -75,10 +75,13 @@ WSGI_APPLICATION = 'myproj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'LearningPlatform',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }}
 
 
 # Password validation
